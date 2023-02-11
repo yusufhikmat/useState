a3bdfd145d16dbccc1f19cc26e48b59d16971407
yusufhikmat <hikmatyusuff2020@gmail.com>
2023-02-11 01:06:06 -1200

    re-write my setCount stae

--- src/components/HookCounter.js
+++ src/components/HookCounter.js
@@ -3,12 +3,19 @@ import React, {useState} from 'react'
 const HookCounter = () => {
     const initialCount = 0
     const [count, setCount] = useState(initialCount)
+
+    const incrementBy5 = () =>{
+        for(let i = 0; i < 5; i++){
+            setCount(prevState => prevState + 1)
+        }
+    }
   return (
     <div>
         <h2>Counter: {count}</h2>
         <button onClick = {()=>setCount(initialCount)}>Reset</button>
-        <button onClick={()=>setCount(count + 1)}>Increment</button>
-        <button onClick={()=>setCount(count - 1)}>Decrement</button>
+        <button onClick={()=>setCount(prevState => prevState + 1)}>Increment</button>
+        <button onClick={()=>setCount(prevState => prevState - 1)}>Decrement</button>
+        <button onClick={incrementBy5}>Increse by 5</button>
     </div>
   )
 }
